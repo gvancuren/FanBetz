7029277f425afacfe935a9cff072e8d9b1faf49a
Gunnar <fanbetzbusiness@gmail.com>
2025-07-16 18:12:51 -0500

    Fix stripe export

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,5 @@
-// src/lib/stripe.ts
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2023-10-16',
 });
-
-export default stripe;
